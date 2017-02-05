0be295f580984112ed8a199f73a783e73c4e7cc4
Sumeet Singh <ortuna@gmail.com>
2017-02-05 18:33:31 -0500

    92% acc

--- even_odd.py
+++ even_odd.py
@@ -1,25 +1,50 @@
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.datasets import load_iris
+import numpy as np
+import random
+
+def int_to_binary(value):
+    return list("{:#018b}".format(i))[2:]
 
 iris_dataset = load_iris()
 knn = KNeighborsClassifier(n_neighbors=1)
 
-values = list()
-answers = list()
-for i in range(1,5000):
-    values.append([i])
+COUNT = 50000
+values = np.zeros((COUNT, 16))
+answers = np.zeros(COUNT, dtype=int)
 
-for value in values:
-    if(value[0] % 2 ==0):
-        answers.append(0)
+for i in range(0,COUNT):
+    v = int_to_binary(i+1)
+    values[i] = np.asarray(v)
+
+for i in range(0,COUNT):
+    if((i+1) % 2 == 0):
+        answers[i] = 1
     else:
-        answers.append(1)
+        answers[i] = 0
 
-X_train, X_test, y_train, y_test = train_test_split(values, answers, random_state=0)
+#print(answers)
 
-# print(X_test)
+#print(X_test)
+#print(knn.predict(X_test))
+
+X_train, X_test, y_train, y_test = train_test_split(
+    values,
+    answers
+)
+
+#print(X_train.shape)
+#print(y_train.shape)
 
 knn.fit(X_train, y_train)
-
 print(knn.score(X_test, y_test))
+
+brand_new = np.zeros((20, 16))
+for i in range(0,20):
+    v = random.randint(1, 500)
+    print(v)
+    brand_new[i] = int_to_binary(v)
+
+predictions = knn.predict(brand_new)
+print(predictions)
